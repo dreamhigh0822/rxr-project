5767cdedd9c1ca62092dc44649637d942233e12d
Dream High <dreamhigh0822@outlook.com>
2019-07-16 14:42:00 -0400

    added modal

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Dropdown, Container, Row, Col } from 'react-bootstrap';
+import { Dropdown, Container, Row, Col, Modal, Button } from 'react-bootstrap';
 import A from './A';
 import Img from './Img';
 import NavBar from './NavBar';
@@ -9,9 +9,17 @@ import messages from './messages';
 import $ from 'jquery';
 
 function Header() {
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleCloseHelp = () => setShow1(false);
+  const handleShow = () => setShow(true);
+  const handleShowHelp = () => setShow1(true);
+
   function onFeauturesMenuDropdown() {
     $('#nav-dropdown').attr('aria-expanded', true);
   }
+
   return (
     <div>
       <div className="va-title-area">
@@ -21,7 +29,7 @@ function Header() {
                 <img src="img/icon-userMenuDown.svg" id="login-off" alt="" aria-hidden="true"/><img src="img/icon-userMenuUp.svg" id="login-on" alt="" aria-hidden="true"/>
             </a>
               <ul className="dropdown-menu dropdown-menu-right" role="navigation" aria-label="User Submenu" id="userMenu">
-                  <li><a href="{{launchpad}}">Return to Launchpad</a></li>
+                  <li><a href="launchpad">Return to Launchpad</a></li>
                   <li><a href="javascript:void(0);" id="logout">Log Out</a></li>
               </ul>
         </div>
@@ -45,8 +53,8 @@ function Header() {
             <li><a href="#prescription-history" >Prescription History</a></li>
             <li><a href="#links">Medication Information on My Health<span className="mhevet">e</span>Vet</a></li>
           </ul>
-          <a href="#" id="about" role="button" data-toggle="modal" data-target="#about-modal" aria-controls="about-modal">About</a>
-          <a href="#" id="help" role="button" data-toggle="modal" data-target="#help-modal" aria-controls="help-modal">Help</a>
+          <a id="about" onClick={handleShow} role="button" data-toggle="modal" data-target="#about-modal" aria-controls="about-modal">About</a>
+          <a id="help" onClick={handleShowHelp} role="button" data-toggle="modal" data-target="#help-modal" aria-controls="help-modal">Help</a>
         </nav>
         <nav className="navbar" id="hamburger-list">
           <a href="#" id="phoneFeaturesMenuLink" className="dropdown-toggle" data-toggle="dropdown" role="button"
@@ -66,6 +74,74 @@ function Header() {
         </nav>
       </div>
 
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>
+          <h4 class="modal-title about-title" id="about-label" tabIndex="0">About</h4>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div class="row about-row">
+              <div class="col-sm-4"><label htmlFor="about-app-name">Application Name</label></div>
+              <div class="col-sm-8" id="about-app-name">app.about.application-name</div>
+          </div>
+          <div class="row about-row">
+              <div class="col-sm-4"><label htmlFor="about-version">Version Number</label></div>
+              <div class="col-sm-8" id="about-version">app.about.version-number</div>
+          </div>
+          <div class="row about-row">
+              <div class="col-sm-4"><label htmlFor="about-developed">Developed By</label></div>
+              <div class="col-sm-8" id="about-developed">app.about.developed-by</div>
+          </div>
+          <div class="row about-row">
+              <div class="col-sm-4"><label htmlFor="about-date">National Release Date</label></div>
+              <div class="col-sm-8" id="about-date">app.about.national-release-date</div>
+          </div>
+          <div class="row about-row" id="about-last">
+              <div class="col-sm-4"><label htmlFor="about-desc">Application Description</label></div>
+              <div class="col-sm-8" id="about-desc">app.about.application-description</div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={handleClose}>Close</button>
+        </Modal.Footer>
+      </Modal>
+      
+      <Modal show={show1} onHide={handleCloseHelp}>
+        <Modal.Header>
+          <Modal.Title>
+          <h4 class="modal-title about-title" id="about-label" tabIndex="0">Help</h4>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row help-row">
+              <div className="col-sm-4"><label for="help-browsers">app.help.browsers.title</label></div>
+              <div className="col-sm-8" id="help-browsers">
+                  This app can be used on the following supported browsers:
+                  <ul>
+                    
+                      <li>browser-name browser-version</li>
+      
+                  </ul>
+              </div>
+          </div>
+          <div className="row help-row">
+              <div className="col-sm-4"><label htmlFor="help-desk">app.help.help-desk.title</label></div>
+              <div className="col-sm-8" id="help-desk">app.help.help-desk.content</div>
+          </div>
+          <div className="row help-row">
+              <div className="col-sm-4"><label htmlFor="help-emergency">app.help.emergency.title</label></div>
+              <div className="col-sm-8" id="help-emergency">app.help.emergency.content</div>
+          </div>
+          <div className="row help-row" id="help-last">
+              <div className="col-sm-4"><label htmlFor="help-feedback">app.help.general-feedback.title</label></div>
+              <div className="col-sm-8" id="help-feedback"><a href="app.help.general-feedback.content" target="_blank">app.help.general-feedback.content</a></div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={handleCloseHelp}>Close</button>
+        </Modal.Footer>
+      </Modal>
   </div>
   );
 }
